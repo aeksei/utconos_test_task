5a773239df459ad474432f0626111f05a2888733
aeksei <aeksei94@gmail.com>
2021-09-20 09:19:02 +0300

    add placeholder for image

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 
-from fastapi import FastAPI, Depends, Form, HTTPException, status
+from fastapi import FastAPI, Depends, Form, HTTPException, status, File, UploadFile
 from sqlalchemy.orm import Session
 from pydantic import ValidationError
 
@@ -33,6 +33,7 @@ async def read_root():
 async def create_user(email: str = Form(...),
                       phone: str = Form(...),
                       comment: str = Form(default=""),
+                      picture: UploadFile = File(default=None),  # fixme check image form field in react app
                       db: Session = Depends(get_db)):
     try:
         user = schemas.AppUserCreate(
@@ -50,5 +51,8 @@ async def create_user(email: str = Form(...),
     db_user = crud.get_user_by_email(db, user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+
+    if picture is not None:
+        ...  # todo save picture in storage
 
     return crud.create_user(db, user)
